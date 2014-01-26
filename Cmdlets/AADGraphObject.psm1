0a4763b5fb884caf6bd703a3d6f0aece80e77aa6
dushyantgill <dushyantgill@gmail.com>
2014-01-25 22:35:44 -0800

    Changes to AADObject
    Invoke-RestMethod has a timeout problem. Changed it to
    Invoke-WebRequest.
    Added new function  Set-AADObjectProperty

--- Cmdlets/AADGraphObject.psm1
+++ Cmdlets/AADGraphObject.psm1
@@ -4,9 +4,12 @@ function Get-AADObject([string]$type) {
     $header = $authenticationResult.CreateAuthorizationHeader()
     $uri = [string]::Format("https://graph.windows.net/{0}/{1}?api-version=2013-04-05",$authenticationResult.TenantId, $type)
     Write-Host HTTP GET $uri -ForegroundColor Cyan
-    $result = Invoke-RestMethod -Method Get -Uri $uri -Headers @{"Authorization"=$header;"Content-Type"="application/json"}
-    if ($result -ne $null) {
-      $objects = $result.Value
+    $result = Invoke-WebRequest -Method Get -Uri $uri -Headers @{"Authorization"=$header;"Content-Type"="application/json"}
+    if($result.StatusCode -eq 200)
+    {
+      Write-Host "Get succeeded." -ForegroundColor Cyan
+      $json = (ConvertFrom-Json $result.Content)
+      if($json -ne $null){$objects = $json.value}
     }
   }
   else{
@@ -21,7 +24,12 @@ function Get-AADObjectById([string]$type, [string]$id) {
     $header = $authenticationResult.CreateAuthorizationHeader()
     $uri = [string]::Format("https://graph.windows.net/{0}/{1}/{2}?api-version=2013-04-05",$authenticationResult.TenantId, $type.Trim(), $id.Trim())
     Write-Host HTTP GET $uri -ForegroundColor Cyan
-    $object = Invoke-RestMethod -Method Get -Uri $uri -Headers @{"Authorization"=$header;"Content-Type"="application/json"}
+    $result = Invoke-WebRequest -Method Get -Uri $uri -Headers @{"Authorization"=$header;"Content-Type"="application/json"}
+    if($result.StatusCode -eq 200)
+    {
+      Write-Host "Get succeeded." -ForegroundColor Cyan
+      $object = (ConvertFrom-Json $result.Content)
+    }
   }
   else{
     Write-Host "Not connected to an AAD tenant. First run Connect-AAD." -ForegroundColor Yellow
@@ -41,7 +49,12 @@ function New-AADObject([string]$type, [object]$object) {
     $byteArray = $enc.GetBytes($body)
     $contentLength = $byteArray.Length
     $headers = @{"Authorization"=$header;"Content-Type"="application/json";"Content-Length"=$contentLength}
-    $newObject = Invoke-RestMethod -Method Post -Uri $uri -Headers $headers -Body $body
+    $result = Invoke-WebRequest -Method Post -Uri $uri -Headers $headers -Body $body
+    if($result.StatusCode -eq 201)
+    {
+      Write-Host "Create succeeded." -ForegroundColor Cyan
+      $newObject = (ConvertFrom-Json $result.Content)
+    }
   }
   else{
     Write-Host "Not connected to an AAD tenant. First run Connect-AAD."
@@ -50,7 +63,6 @@ function New-AADObject([string]$type, [object]$object) {
 }
 
 function Set-AADObject([string]$type, [string]$id, [object]$object) {
-  $updatedObject = $null
   if($global:authenticationResult -ne $null) {
     $header = $authenticationResult.CreateAuthorizationHeader()
     $uri = [string]::Format("https://graph.windows.net/{0}/{1}/{2}?api-version=2013-04-05",$authenticationResult.TenantId, $type, $id)
@@ -61,44 +73,89 @@ function Set-AADObject([string]$type, [string]$id, [object]$object) {
     $byteArray = $enc.GetBytes($body)
     $contentLength = $byteArray.Length
     $headers = @{"Authorization"=$header;"Content-Type"="application/json";"Content-Length"=$contentLength}
-    $newObject = Invoke-RestMethod -Method Patch -Uri $uri -Headers $headers -Body $body
+    $result = Invoke-WebRequest -Method Patch -Uri $uri -Headers $headers -Body $body
+    if($result.StatusCode -eq 204)
+    {
+      Write-Host "Update succeeded." -ForegroundColor Cyan
+    }
   }
   else{
     Write-Host "Not connected to an AAD tenant. First run Connect-AAD." -ForegroundColor Yellow
   }
-  return $updatedObject
 }
 
 function Remove-AADObject([string]$type, [string]$id) {
-  $deleteResult = $null
   if($global:authenticationResult -ne $null) {
     $header = $authenticationResult.CreateAuthorizationHeader()
     $uri = [string]::Format("https://graph.windows.net/{0}/{1}/{2}?api-version=2013-04-05",$authenticationResult.TenantId, $type, $id)
     Write-Host HTTP DELETE $uri -ForegroundColor Cyan
     $headers = @{"Authorization"=$header;"Content-Type"="application/json"}
-    $deleteResult = Invoke-RestMethod -Method Delete -Uri $uri -Headers $headers
+    $result = Invoke-WebRequest -Method Delete -Uri $uri -Headers $headers
+    if($result.StatusCode -eq 204)
+    {
+      Write-Host "Delete succeeded." -ForegroundColor Cyan
+    }
   }
   else{
     Write-Host "Not connected to an AAD tenant. First run Connect-AAD." -ForegroundColor Yellow
   }
-  return $deleteResult
 }
 
-function Get-AADLinkedObject([string]$type, [string] $id, [string]$relationship, [bool]$linksOnly) {
+function Get-AADLinkedObject([string]$type, [string] $id, [string]$relationship, [bool]$getLinksOnly) {
   $objects = $null
   if($global:authenticationResult -ne $null){
     $header = $authenticationResult.CreateAuthorizationHeader()
-    $uri = ""
-    if($linksOnly) {$uri = [string]::Format("https://graph.windows.net/{0}/{1}/{2}/$links/{3}?api-version=2013-04-05",$authenticationResult.TenantId, $type, $id, $relationship)}
+    $uri = $null
+    if($getLinksOnly) {$uri = [string]::Format("https://graph.windows.net/{0}/{1}/{2}/$links/{3}?api-version=2013-04-05",$authenticationResult.TenantId, $type, $id, $relationship)}
     else {$uri = [string]::Format("https://graph.windows.net/{0}/{1}/{2}/{3}?api-version=2013-04-05",$authenticationResult.TenantId, $type, $id, $relationship)}
     Write-Host HTTP GET $uri -ForegroundColor Cyan
-    $result = Invoke-RestMethod -Method Get -Uri $uri -Headers @{"Authorization"=$header;"Content-Type"="application/json"}
-    if ($result -ne $null) {
-      $objects = $result.Value
+    $result = Invoke-WebRequest -Method Get -Uri $uri -Headers @{"Authorization"=$header;"Content-Type"="application/json"}
+    if($result.StatusCode -eq 200)
+    {
+      Write-Host "Get succeeded." -ForegroundColor Cyan
+      $json = (ConvertFrom-Json $result.Content)
+      if($json -ne $null){$objects = $json.value}
     }
   }
   else{
     Write-Host "Not connected to an AAD tenant. First run Connect-AAD." -ForegroundColor Yellow
   }
   return $objects
+}
+
+function Set-AADObjectProperty([string]$type, [string] $id, [string]$property, [object]$value, [bool]$isLinked, [string]$contentType) {
+  if($global:authenticationResult -ne $null) {
+    $header = $authenticationResult.CreateAuthorizationHeader()
+    $uri = $null
+    if($isLinked) {$uri = [string]::Format('https://graph.windows.net/{0}/{1}/{2}/$links/{3}?api-version=2013-04-05',$authenticationResult.TenantId, $type, $id, $property)}
+    else {$uri = [string]::Format('https://graph.windows.net/{0}/{1}/{2}/{3}?api-version=2013-04-05',$authenticationResult.TenantId, $type, $id, $property)}
+    Write-Host HTTP PUT $uri -ForegroundColor Cyan
+
+    $body = $null
+    $byteArray = $null
+    
+    if($contentType.Trim() -eq "" -or $contentType -eq $null -or $contentType.ToLower() -eq "application/json") {
+      $contentType = "application/json"
+      $enc = New-Object "System.Text.ASCIIEncoding"
+      $body = ConvertTo-Json -InputObject $value
+      $byteArray = $enc.GetBytes($body)
+      Write-Host $body -ForegroundColor Cyan
+    }
+    elseif ($contentType.ToLower() -eq "image/jpeg" -or $contentType.ToLower() -eq "image/png" -or $contentType.ToLower() -eq "image/gif") {
+      $contentType = $contentType.ToLower()
+      $body = $value
+      $byteArray = $value
+      Write-Host "Body of the request is binary data." -ForegroundColor Cyan
+    }
+    $contentLength = $byteArray.Length
+    $headers = @{"Authorization"=$header;"Content-Type"=$contentType;"Content-Length"=$contentLength}
+    $result = Invoke-WebRequest -Method Put -Uri $uri -Headers $headers -Body $body
+    if($result.StatusCode -eq 204)
+    {
+      Write-Host "Update succeeded." -ForegroundColor Cyan
+    }
+  }
+  else{
+    Write-Host "Not connected to an AAD tenant. First run Connect-AAD." -ForegroundColor Yellow
+  }
 }
